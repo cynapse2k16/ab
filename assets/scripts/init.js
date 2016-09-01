2544252aac379ef8a4cb149e42a013ab6153d400
R.M.K. Engineering College <rmkec@RMKs-iMac-2.local>
2016-09-01 15:40:30 +0530

    added contacts

---
+++ assets/scripts/init.js
@@ -0,0 +1,8 @@
+(function($){
+  angular.element(function(){
+
+    angular.element('.button-collapse').sideNav();
+    angular.element('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
